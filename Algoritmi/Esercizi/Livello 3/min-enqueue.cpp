e11a70ab39b15f7f97972ce81d2258ab631e7e7b
Danilo <danilo.santitto@gmail.com>
2019-04-02 17:24:28 +0200

    prima versione

---
+++ Algoritmi/Esercizi/Livello 3/min-enqueue.cpp
@@ -0,0 +1,17 @@
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+template<class H> class CodaPrioria{
+    public:
+    virtual enqueue(H* a) = 0;
+    virtual heapify(int i) = 0;
+    virtual buildHeap() = 0;
+};
+template<class H> class BinaryHeap{
+    private:
+    
+
+
+};
