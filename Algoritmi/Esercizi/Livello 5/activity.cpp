88dde01dd910eccde8714564d3f0a6e59790bbdd
Danilo <danilo.santitto@gmail.com>
2019-04-22 17:54:18 +0200

    reso funzionante

--- Algoritmi/Esercizi/Livello 5/activity.cpp
+++ Algoritmi/Esercizi/Livello 5/activity.cpp
@@ -1,5 +1,9 @@
+/*
+    Viene considerata una tabella con numero di righe = all'ultimo tempo di fine e colonne = al numero di attività. L'algoritmo considera le righe come lo scorrere del tempo per le attività
+    mentre le colonne come le attività stesse. Il passo consisten nel considerare la cella precedente se il tempo + minore del tempo di fine dell'attività analizzata correntemente, altrimenti 
+    viene considerato il massimo trà la cella precedente a sinistra e la cella di riga corrispondente al tempo di inizio dell'attività precedente + la durata dell'attività corrente.
+*/
 #include<fstream>
-#include<iostream>
 #include<string>
 #include<sstream>
 
@@ -9,32 +13,48 @@ class Activity{
     private:
         int* inizio;
         int* fine;
+        int* durata;
     public:
-        Activity(int i, int f) : inizio(new int(i)), fine(new int(f)){}
+        Activity(int i, int f) : inizio(new int(i)), fine(new int(f)), durata(new int(f-i)){}
         int getInizio(){ return *inizio; }
         int getFine(){ return *fine; }
+        int getDurata(){ return *durata; }
 };
 
-bool compatible(Activity* a, Activity* b){
-    return ((b->getInizio() <= a->getInizio() || b->getInizio() >= a->getFine()) && 
-            (b->getFine() <= a->getInizio() || b->getFine() >= a->getFine()));
+void countingSort(Activity** A, int N){
+    int max = A[0]->getFine();
+    int min = A[0]->getFine();
+    for(int i = 0; i < N; i++){
+        if(A[i]->getFine() > max) max = A[i]->getFine();
+        if(A[i]->getFine() < min) min = A[i]->getFine();
+    }
+
+    int len = max - min + 1;
+    int C[len] = {0};
+    for(int i = 0; i < N; i++) C[A[i]->getFine()-min]++;
+    for(int i = 1; i < len; i++) C[i] += C[i-1];
+
+    Activity** B = new Activity*[N];
+    for(int i = N-1; i >= 0; i--) B[(C[A[i]->getFine()-min]--) - 1] = A[i];
+
+     for(int i = 0; i < N; i++) A[i] = B[i];
 }
 
  int maxActivity(Activity** A, int N){
-    int** table = new int*[N+1];
-    for(int i = 0; i <= N; i++){
+    int len = A[N-1]->getFine()+1;
+    int** table = new int*[len];
+    for(int i = 0; i < len; i++){
         table[i] = new int[N+1];
-        table[0][i] = table[i][0] = 1;
+        table[i][0] = 0;
     }
-
+    for(int j = 0; j <= N; j++) table[0][j] = 0;
     
-    for(int i = 1; i <= N; i++){
+    for(int i = 1; i < len; i++){
         for(int j = 1; j <= N; j++){
-            
-            if(A[i-1]->getInizio() == A[j-1]->getInizio() && A[i-1]->getFine() == A[j-1]->getFine())
-                table[i][j] = table[i][j-1];
-            else if(compatible(A[i-1], A[j-1])) table[i][j] = table[i][j-1] + 1;
-            else table[i][j] = max(table[i-1][j], table[i][j-1]);
+            if(i<A[j-1]->getFine())
+                table[i][j]=table[i][j-1];
+            else
+                table[i][j]=max(table[i][j-1],table[A[j-1]->getInizio()][j-1] + A[j-1]->getDurata());
         }
     }
     /* for(int k = 0; k <= N; k++){
@@ -44,9 +64,7 @@ bool compatible(Activity* a, Activity* b){
     cout << endl;
     }
     cout << endl; */
-    
-
-    return table[N][N];
+    return table[len-1][N];
 } 
 int main(){
     const int DIM = 100;
@@ -59,17 +77,13 @@ int main(){
         for(int j = 0; j < N; j++){
             string s1, s2;
             in >> s1 >> s2;
-            stringstream s;
-            s << s1.substr(1, s1.length());
-            int val1;
-            s >> val1;
-            s.clear();
-            s << s2.substr(0, s1.length()-1);
-            int val2;
-            s >> val2;
+            int val1, val2;
+            stringstream(s1.substr(1, s1.length())) >> val1;
+            stringstream(s2.substr(0, s2.length()-1)) >> val2;
             A[j] = new Activity(val1, val2);  
         }
-        out << maxActivity(A, N) <<endl;
+        countingSort(A, N);
+        out << maxActivity(A, N) << endl;
         delete [] A;
     }
     in.close();
