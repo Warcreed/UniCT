212b9b851fd98104ab58b61b37f2c2efc60f2fa9
Danilo Santitto <danilo.santitto@gmail.com>
2019-11-04 15:22:34 +0100

    Fixati problemi relativi a segmentation fault

--- Tecnologie per i sistemi distribuiti e il web con laboratorio/TiroAllaFune.c
+++ Tecnologie per i sistemi distribuiti e il web con laboratorio/TiroAllaFune.c
@@ -6,7 +6,7 @@
 
 pthread_mutex_t player_one = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t player_two = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+// pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //ho dei dubbi sulla reale utilità di questa mutex
 
 int posizione = 0;
 int vittorie_tp0 = 0;
@@ -14,22 +14,23 @@ int vittorie_tp1 = 0;
 
 void first_player()
 {
-    while (vittorie_tp0 < 10 || vittorie_tp1 < 10)
+    while (vittorie_tp0 < 10 && vittorie_tp1 < 10)
     {
+        printf("Primo giocatore: %d\n", posizione);
         int recupero = rand() % 4;
         int forza = rand() % 6;
         sleep(recupero);
         if (posizione >= 10)
         {
-            pthread_mutex_lock(&mutex);
+            // pthread_mutex_lock(&mutex);
             ++vittorie_tp1;
             posizione = 0;
-            pthread_mutex_unlock(&mutex);
+            // pthread_mutex_unlock(&mutex);
             pthread_mutex_unlock(&player_two);
         }else{
-            pthread_mutex_lock(&mutex);
+            // pthread_mutex_lock(&mutex);
             posizione -= forza;
-            pthread_mutex_unlock(&mutex);
+            // pthread_mutex_unlock(&mutex);
             if(posizione <= -10)
                 pthread_mutex_lock(&player_one);
         }   
@@ -39,22 +40,23 @@ void first_player()
 
 void second_player()
 {
-    while (vittorie_tp1 < 10 || vittorie_tp0 < 10)
+    while (vittorie_tp1 < 10 && vittorie_tp0 < 10)
     {
+        printf("Secondo giocatore: %d\n", posizione);
         int recupero = rand() % 4;
         int forza = rand() % 6;
         sleep(recupero);
         if (posizione <= -10)
         {
-            pthread_mutex_lock(&mutex);
+            // pthread_mutex_lock(&mutex);
             ++vittorie_tp0;
             posizione = 0;
-            pthread_mutex_unlock(&mutex);
+            // pthread_mutex_unlock(&mutex);
             pthread_mutex_unlock(&player_one);
         }else{
-           pthread_mutex_lock(&mutex);
+            // pthread_mutex_lock(&mutex);
             posizione += forza;
-            pthread_mutex_unlock(&mutex);
+            // pthread_mutex_unlock(&mutex);
             if(posizione >= 10)
                 pthread_mutex_lock(&player_two);
         }   
@@ -66,14 +68,14 @@ int main(int argc, char *argv[])
 {
     pthread_t tp[2];
     srand(time(NULL));
-    pthread_create(&tp[0], NULL, (void*) &player_one, ((void *)0));
-    pthread_create(&tp[1], NULL, (void*) &player_two, ((void *)0));
+    pthread_create(&tp[0], NULL, (void*) &first_player, ((void *)0));
+    pthread_create(&tp[1], NULL, (void*) &second_player, ((void *)0));
 
     pthread_join(tp[0], NULL);
     pthread_join(tp[1], NULL);
     if(vittorie_tp0 > vittorie_tp1)
-        printf("first player ha vinto con %d vittorie", vittorie_tp0);
+        printf("first player ha vinto con %d vittorie\n", vittorie_tp0);
     else
-        printf("second player ha vinto con %d vittorie", vittorie_tp1);
+        printf("second player ha vinto con %d vittorie\n", vittorie_tp1);
     exit(0);
 }
